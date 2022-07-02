4cf5f7563b18cf54240e9bc8ea32b3e63661b047
Segun Akinola <akinolasegun99@yahoo.com>
2022-07-02 12:51:28 -0700

    my task 4 amend

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
-nclude <stdio.h>
-
-/
-
- * main - Entry point
- *
+#include <stdio.h>
+/*main - Entry point
  *  * Return: Always 0 (sucess)
  *
- *   */
-
- int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
+ * 
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
-
-
