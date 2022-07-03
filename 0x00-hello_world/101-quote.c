aaf8aea20406d642fbbd7b623c1d85a2537f5ffa
Segun Akinola <akinolasegun99@yahoo.com>
2022-07-03 12:19:20 -0700

    My task 8 amend 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
 #include <stdlib.h>
+
 /**
  *
  *  * main - Entry point
@@ -8,9 +8,11 @@
  *   * Return: Always 1 (Success)
  *
  *    */
+
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
+
+	write(STDOUT_FILENO, "and that piece of art is useful
 							\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
