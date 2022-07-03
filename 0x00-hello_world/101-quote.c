9923b00e8b89f4413656709d1b4888433f814cfc
Segun Akinola <akinolasegun99@yahoo.com>
2022-07-03 12:25:32 -0700

    My task 8 original

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  *
  *  * main - Entry point
@@ -10,9 +9,12 @@
  *    */
 
 int main(void)
+
 {
 
-	write(STDOUT_FILENO, "and that piece of art is useful
+		write(STDOUT_FILENO, "and that piece of art is useful
+
 							\" - Dora Korpar, 2015-10-19\n", 59);
+
 			return (1);
 }
